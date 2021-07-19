7b6c9619a2e092efeed8ff192360aa0c0c639e87
John Lloyd Patrick Japos <dyollsopaj@gmail.com>
2021-07-19 15:49:47 +0800

    initial design of mobile responsive

--- js/team.js
+++ js/team.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-	const w1100up = () => {
+	const w800up = () => {
 		const slideDownBtn = document.querySelectorAll('#team .slide-down-btn');
 		Array.from(slideDownBtn).forEach(btn => {
 			btn.addEventListener('click', e => {
@@ -30,21 +30,62 @@ window.onload = () => {
 		searchName();
 	}
 
-	const w1100down = () => {
-		console.log('width is less than 1100');
+	const w800down = () => {
+		const slideDownBtn = document.querySelectorAll('#team .slide-down-btn');
+		Array.from(slideDownBtn).forEach(btn => {
+			btn.addEventListener('click', e => {
+				if (e.target.nodeName === 'I') {
+					const btn = e.target;
+					const downloadBtn = btn.parentElement.parentElement.children[3];
+					downloadBtn.classList.toggle('show');
+				} else {
+					console.warn('warning error');
+				}
+			});
+		});
+		const searchName = () => {
+			const input = document.querySelector('#search-name');
+			const team = document.querySelector('#team');
+			input.addEventListener('keyup', e => {
+				let inputValue = input.value.toLowerCase();
+				for (let i = 0; i < team.children.length; i++) {
+					let name = team.children[i].children[0].textContent.toLowerCase();
+					if (name.indexOf(inputValue) !== -1) {
+						team.children[i].style.display = 'initial';
+					} else {
+						team.children[i].style.display = 'none';
+					}
+				}
+			});
+		}
+		const mobileNavFunction = () => {
+			const mobileNav = document.querySelector('.mobile-nav');
+			const mobileNavBtn = document.querySelector('.mobile-nav-btn');
+			const mobileNavList = document.querySelector('.mobile-nav-list');
+
+			mobileNavBtn.addEventListener('click', () => {
+				mobileNavList.classList.toggle('show');
+			})
+
+			window.addEventListener('scroll', () => {
+				mobileNavList.classList.remove('show');
+			})
+		}
+		searchName();
+		mobileNavFunction();
 	}
 
 	if (window.outerWidth >= 800) {
-		w1100up();
+		w800up();
 	} else {
-		w1100down();
+		w800down();
 	}
 
 	window.addEventListener('resize', () => {
 		if (window.outerWidth >= 800) {
-			w1100up();
+			w800up();
 		} else {
-			w1100down();
+			w800down();
 		}
 	})
 
